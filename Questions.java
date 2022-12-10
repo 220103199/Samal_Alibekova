2728be1a6c885f2e12284e2242b9a3a1be78210e
220103199 <120212100+220103199@users.noreply.github.com>
2022-12-10 11:26:14 +0600

    Create Questions.java

---
+++ Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Samal");
+        System.out.println("Surname: Alibekova");
+        System.out.println("Id: 220103199");
+        System.out.println("Hobby: Basketball");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 5");
+        System.out.println("My favorite color?: Yellow");
+    }
+}
